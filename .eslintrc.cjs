10c3f78d97604d08900aa897fda5ff56208af1e7
eunbinnie <dmsqlsdl54@gmail.com>
2025-03-25 23:42:50 +0900

    init: 프로젝트 초기 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'eslint-plugin-import-helpers',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'newline-before-return': 2,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'import-helpers/order-imports': [
+      2,
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          ['module'],
+          '/^@/styles/',
+          '/^@/lib/',
+          '/^@/utils/',
+          '/^@/types/',
+          '/^@/components/',
+          ['parent', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-object-type': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return <Routes>{/* <Route path='/' element={<Home />} /> */}</Routes>;
+}
+
+export default App;
