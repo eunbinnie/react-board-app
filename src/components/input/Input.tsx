90bcaa4d98e7013d1318a12c062610b95d7297a9
eunbinnie <dmsqlsdl54@gmail.com>
2025-03-26 21:19:20 +0900

    feat: 비밀번호 타입 Input 가시성 토글 기능 추가

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,6 +1,9 @@
-import { forwardRef } from 'react';
+import { forwardRef, useCallback, useState } from 'react';
 
 import { cn } from '@/utils/cn';
+
+import VisibilityOff from '/icons/visibility-off.svg';
+import VisibilityOn from '/icons/visibility-on.svg';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
@@ -15,18 +18,41 @@ export default forwardRef(function Input(
   { type, className, ...rest }: InputProps,
   ref: React.LegacyRef<HTMLInputElement>,
 ) {
+  const [passwordToggle, setPasswordToggle] = useState(false);
+  const isPassword = type === 'password';
+  const newType = passwordToggle && isPassword ? 'text' : type;
+
+  // 비밀번호 가시성 아이콘 토글
+  const handlePasswordToggle = useCallback(() => {
+    setPasswordToggle((prev) => !prev);
+  }, []);
+
   return (
     <div className='grid'>
       <div className='relative'>
         <input
-          type={type}
+          type={newType}
           className={cn(
             'w-full rounded border border-solid border-gray-500 px-5 py-4 leading-[1.6] text-black outline-none placeholder:text-gray-500',
+            isPassword ? 'pr-[54px]' : 'pr-5',
             className,
           )}
           ref={ref}
           {...rest}
         />
+        {isPassword && (
+          <button
+            type='button'
+            className='absolute right-5 top-1/2 -translate-y-1/2'
+            onClick={handlePasswordToggle}
+            aria-label={passwordToggle ? '비밀번호 숨기기' : '비밀번호 보기'}
+          >
+            <img
+              src={passwordToggle ? VisibilityOn : VisibilityOff}
+              alt={passwordToggle ? '비밀번호 숨기기' : '비밀번호 보기'}
+            />
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Input from '@/components/input/Input';
 
 const HomePage = () => {
-  return <Input />;
+  return <Input type='password' />;
 };
 
 export default HomePage;
