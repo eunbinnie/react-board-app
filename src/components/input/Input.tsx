aa21263d851b6cbeaf52677f88c20c888100b624
이은빈 <110139747+eunbinnie@users.noreply.github.com>
2025-03-26 21:26:35 +0900

    feat: input 공통 컴포넌트 작업 (#2)
    * feat: 클래스 네임 병합 cn 함수 추가
    
    * fix: 절대경로 에러 해결 (vite, tsconfig.app에 추가)
    
    * feat: input 컴포넌트 기본 스타일링 작업
    
    * feat: 비밀번호 타입 Input 가시성 토글 기능 추가
    
    * feat: input error 타입 추가 및 errorMessage 조건부렌더링
    
    * refactor: input id 추가 및 props 타입 개선
    
    - error 기본값 false로 설정
    - ref 타입 변경

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,69 @@
+import { forwardRef, useCallback, useState } from 'react';
+
+import { cn } from '@/utils/cn';
+
+import VisibilityOff from '/icons/visibility-off.svg';
+import VisibilityOn from '/icons/visibility-on.svg';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  errorMessage?: string;
+  className?: string;
+}
+
+/**
+ * input 공통 컴포넌트
+ * @param type input type
+ * @param error 값이 true일 경우 error 스타일 활성화 아니라면 false (기본값 false 설정 필요)
+ * @param errorMessage input 밑에 보여줄 에러 메세지
+ * @param className input 커스텀 className
+ */
+export default forwardRef(function Input(
+  { type, id, error = false, errorMessage, className, ...rest }: InputProps,
+  ref: React.ForwardedRef<HTMLInputElement>,
+) {
+  const [passwordToggle, setPasswordToggle] = useState(false);
+  const isPassword = type === 'password';
+  const newType = passwordToggle && isPassword ? 'text' : type;
+
+  // 비밀번호 가시성 아이콘 토글
+  const handlePasswordToggle = useCallback(() => {
+    setPasswordToggle((prev) => !prev);
+  }, []);
+
+  return (
+    <div className='grid'>
+      <div className='relative'>
+        <input
+          id={id}
+          type={newType}
+          className={cn(
+            'w-full rounded border border-solid border-gray-500 px-5 py-4 leading-[1.6] text-black outline-none placeholder:text-gray-500',
+            isPassword ? 'pr-[54px]' : 'pr-5',
+            className,
+          )}
+          ref={ref}
+          {...rest}
+        />
+        {isPassword && (
+          <button
+            type='button'
+            className='absolute right-5 top-1/2 -translate-y-1/2'
+            onClick={handlePasswordToggle}
+            aria-label={passwordToggle ? '비밀번호 숨기기' : '비밀번호 보기'}
+          >
+            <img
+              src={passwordToggle ? VisibilityOn : VisibilityOff}
+              alt={passwordToggle ? '비밀번호 숨기기' : '비밀번호 보기'}
+            />
+          </button>
+        )}
+      </div>
+      {error && errorMessage && (
+        <span className='mt-2 block pl-2 text-xs leading-[1.3] text-red-500'>
+          {errorMessage}
+        </span>
+      )}
+    </div>
+  );
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import Input from '@/components/input/Input';
+
 const HomePage = () => {
-  return <div>메인 홈 페이지</div>;
+  return <Input type='password' error={true} errorMessage='에러 메세지' />;
 };
 
 export default HomePage;
