2d34954ca0246ba5fac529afc765aaf808572a45
eunbinnie <dmsqlsdl54@gmail.com>
2025-03-27 18:47:39 +0900

    feat: auth(로그인, 회원가입) 페이지 레이아웃 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import SignUpPage from './pages/auth/SignUp.tsx';
 import HomePage from './pages/Home.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />,
   },
 ]);
 
---
+++ src/pages/auth/SignUp.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from './AuthLayout';
+
+const SignUpPage = () => {
+  return (
+    <AuthLayout>
+      <div>회원가입</div>
+    </AuthLayout>
+  );
+};
+
+export default SignUpPage;
