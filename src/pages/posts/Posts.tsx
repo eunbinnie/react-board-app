ff4760884258faed5f47f1cecf002c10b935b94c
eunbinnie <dmsqlsdl54@gmail.com>
2025-03-30 02:28:39 +0900

    feat: 게시글 목록 화면 구현 및 작성일 한국 시간으로 포맷팅

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -3,10 +3,15 @@ import { useNavigate } from 'react-router-dom';
 
 import { useGetPostsQuery } from '@/services/postApi';
 import type { RootState } from '@/store/store';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
 import Button from '@/components/button/Button';
 
 const PostListPage = () => {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
   const navigate = useNavigate();
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated,
@@ -14,10 +19,6 @@ const PostListPage = () => {
   const { data, error } = useGetPostsQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
-
-  if (data) {
-    console.log(data);
-  }
 
   if (error) {
     console.error(error);
@@ -30,7 +31,31 @@ const PostListPage = () => {
           <h3 className='text-2xl font-bold'>게시글 목록</h3>
         </div>
         {isAuthenticated ? (
-          <div className='mt-10 grid gap-5'>post list</div>
+          <div className='mt-10 grid border-t border-gray-300'>
+            <div className='flex items-center border-b py-2'>
+              <div className='w-[calc(100%-220px)] flex-1 text-center'>
+                제목
+              </div>
+              <div className='w-[100px] truncate text-center'>작성자</div>
+              <div className='w-[120px] text-center'>작성일</div>
+            </div>
+            {data?.map((item) => (
+              <div
+                key={item.id}
+                className='flex items-center overflow-hidden border-b py-2'
+              >
+                <div className='w-[calc(100%-220px)] flex-1 truncate'>
+                  {item.title}
+                </div>
+                <div className='w-[100px] truncate text-center'>
+                  {item.author}
+                </div>
+                <div className='w-[120px] text-center'>
+                  {dayjs(item.created_at).tz('Asia/Seoul').format('YYYY-MM-DD')}
+                </div>
+              </div>
+            ))}
+          </div>
         ) : (
           <div className='mt-[500px] flex flex-col items-center gap-6'>
             <span>로그인 후 게시글 목록을 확인할 수 있어요.</span>
